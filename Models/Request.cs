2829a9bf53c699544120710a47f3f40523037974
iheb abbassi <92274939+ihebabbassi@users.noreply.github.com>
2022-05-13 18:51:12 +0200

    add models

---
+++ Models/Request.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TeamManagement.Models
+{
+    public class Request
+    {
+        [Column("RequestId")]
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Required]
+        public int RequestId { get; set; }
+
+        [Column("Type")]
+        [Required]
+        [StringLength(50)]
+        public string Type { get; set; }
+
+        [Column("Description")]
+        [Required]
+        [StringLength(50)]
+        public string Description { get; set; }
+
+        [Column("Date")]
+        [Required]
+        [StringLength(50)]
+        public string Date { get; set; }
+
+        [Column("Status")]
+        [Required]
+        [StringLength(50)]
+        public string Status { get; set; }
+
+        [ForeignKey("EmployeeId")]
+        public virtual Employee Employee { get; set; }
+
+    }
+}
