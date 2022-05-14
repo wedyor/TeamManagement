9cb23f3c27a6a2f2e9329a013e8c7fcb84a2cd0d
iheb abbassi <92274939+ihebabbassi@users.noreply.github.com>
2022-05-14 20:30:14 +0200

    add employees crud

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TeamManagement.Models;
+using TeamManagement.Repositories;
+
+namespace TeamManagement.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly ITeamRepository _teamRepository;
+
+        public EmployeeController(ITeamRepository teamRepository)
+        {
+            _teamRepository = teamRepository;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Employee>> GetEmployees()
+        {
+            return await _teamRepository.Get();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Employee>> GetEmployees(int id)
+        {
+            return await _teamRepository.Get(id);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Employee>> PostEmployees([FromBody] Employee employee)
+        {
+            var newEmployee = await _teamRepository.Create(employee);
+            return CreatedAtAction(nameof(GetEmployees), new { id = newEmployee.EmployeeId }, newEmployee);
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> PutBooks(int id, [FromBody] Employee employee)
+        {
+            if (id != employee.EmployeeId)
+            {
+                return BadRequest();
+            }
+
+            await _teamRepository.Update(employee);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var employeeToDelete = await _teamRepository.Get(id);
+            if (employeeToDelete == null)
+                return NotFound();
+
+            await _teamRepository.Delete(employeeToDelete.EmployeeId);
+            return NoContent();
+        }
+    }
+}
---
+++ Repositories/TeamRepository.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TeamManagement.DataLayer;
+using TeamManagement.Models;
+
+namespace TeamManagement.Repositories
+{
+    public class TeamRepository : ITeamRepository
+    {
+        private readonly TeamContext _context;
+        public TeamRepository(TeamContext context)
+        {
+            this._context = context;
+        }
+        public async Task<Employee> Create(Employee employee)
+        {
+            _context.Employees.Add(employee);
+            await _context.SaveChangesAsync();
+
+            return employee;
+        }
+
+        public async Task Delete(int id)
+        {
+            var employeeToDelete = await _context.Employees.FindAsync(id);
+            _context.Employees.Remove(employeeToDelete);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<Employee>> Get()
+        {
+            return await _context.Employees.ToListAsync();
+        }
+
+        public async Task<Employee> Get(int id)
+        {
+            return await _context.Employees.FindAsync(id);
+        }
+
+        public async Task Update(Employee employee)
+        {
+            _context.Entry(employee).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+        }
+    }
+}
