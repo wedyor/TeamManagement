2a84099beee4002035ea89985f2d155ea83e7aa4
iheb abbassi <92274939+ihebabbassi@users.noreply.github.com>
2022-05-17 23:27:26 +0200

    update migrations

---
+++ Migrations/20220517192151_fix-json.cs
@@ -0,0 +1,128 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TeamManagement.Migrations
+{
+    public partial class fixjson : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Requests_Users_UserId",
+                table: "Requests");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Users",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Users");
+
+            migrationBuilder.RenameColumn(
+                name: "Role",
+                table: "Users",
+                newName: "role");
+
+            migrationBuilder.RenameColumn(
+                name: "Password",
+                table: "Users",
+                newName: "password");
+
+            migrationBuilder.RenameColumn(
+                name: "LastName",
+                table: "Users",
+                newName: "lastname");
+
+            migrationBuilder.RenameColumn(
+                name: "FirstName",
+                table: "Users",
+                newName: "firstname");
+
+            migrationBuilder.RenameColumn(
+                name: "Email",
+                table: "Users",
+                newName: "email");
+
+            migrationBuilder.AddColumn<int>(
+                name: "id",
+                table: "Users",
+                nullable: false,
+                defaultValue: 0)
+                .Annotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Users",
+                table: "Users",
+                column: "id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Requests_Users_UserId",
+                table: "Requests",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Requests_Users_UserId",
+                table: "Requests");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Users",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "id",
+                table: "Users");
+
+            migrationBuilder.RenameColumn(
+                name: "role",
+                table: "Users",
+                newName: "Role");
+
+            migrationBuilder.RenameColumn(
+                name: "password",
+                table: "Users",
+                newName: "Password");
+
+            migrationBuilder.RenameColumn(
+                name: "lastname",
+                table: "Users",
+                newName: "LastName");
+
+            migrationBuilder.RenameColumn(
+                name: "firstname",
+                table: "Users",
+                newName: "FirstName");
+
+            migrationBuilder.RenameColumn(
+                name: "email",
+                table: "Users",
+                newName: "Email");
+
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "Users",
+                type: "int",
+                nullable: false,
+                defaultValue: 0)
+                .Annotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Users",
+                table: "Users",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Requests_Users_UserId",
+                table: "Requests",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "UserId",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
---
+++ Migrations/20220517192351_fix-json-2.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TeamManagement.Migrations
+{
+    public partial class fixjson2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Requests_Users_UserId",
+                table: "Requests");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Requests_UserId",
+                table: "Requests");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Requests");
+
+            migrationBuilder.AddColumn<int>(
+                name: "id",
+                table: "Requests",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Requests_id",
+                table: "Requests",
+                column: "id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Requests_Users_id",
+                table: "Requests",
+                column: "id",
+                principalTable: "Users",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Requests_Users_id",
+                table: "Requests");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Requests_id",
+                table: "Requests");
+
+            migrationBuilder.DropColumn(
+                name: "id",
+                table: "Requests");
+
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "Requests",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Requests_UserId",
+                table: "Requests",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Requests_Users_UserId",
+                table: "Requests",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
